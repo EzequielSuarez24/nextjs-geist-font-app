f183d72b1131defad943320c99c9bfb9c7175576
Ezequiel Suarez <112830760+EzequielSuarez24@users.noreply.github.com>
2025-09-08 11:19:32 -0300

    Add universidad/src/main/java/com/example/universidad/dto/AsignaturaDTO.java 1757341171581

---
+++ universidad/src/main/java/com/example/universidad/dto/AsignaturaDTO.java
@@ -0,0 +1,43 @@
+package com.example.universidad.dto;
+
+import com.example.universidad.model.Estado;
+
+public class AsignaturaDTO {
+    private Long id;
+    private Estado estado;
+    private Long alumnoId;
+    private Long materiaId;
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Long getAlumnoId() {
+        return alumnoId;
+    }
+
+    public void setAlumnoId(Long alumnoId) {
+        this.alumnoId = alumnoId;
+    }
+
+    public Long getMateriaId() {
+        return materiaId;
+    }
+
+    public void setMateriaId(Long materiaId) {
+        this.materiaId = materiaId;
+    }
+}
