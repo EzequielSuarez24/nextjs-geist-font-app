75f589962e91272da6d180719503a056ce0899c5
Ezequiel Suarez <112830760+EzequielSuarez24@users.noreply.github.com>
2025-09-08 11:19:19 -0300

    Add universidad/src/main/java/com/example/universidad/dto/LoginResponse.java 1757341158746

---
+++ universidad/src/main/java/com/example/universidad/dto/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.example.universidad.dto;
+
+public class LoginResponse {
+    private String token;
+    private String role;
+
+    // getters and setters
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
